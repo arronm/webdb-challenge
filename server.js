d9b1e3a56c4f39d4fdfc26f328911de741c6e134
Arron M <marshall.arron@gmail.com>
2019-06-07 09:49:12 -0700

    Refactor server to use actions and projects routers

--- server.js
+++ server.js
@@ -5,11 +5,8 @@ const cors = require('cors');
 const server = express();
 const PORT = 4444;
 
-const projectDB = require('./data/project-models');
-const actionDB = require('./data/action-models');
-const errorRef = require('./helpers/errorRef');
-const validateBody = require('./middleware/validateBody');
-const validateId = require('./middleware/validateId');
+const projectRouter = require('./routers/projects');
+const actionRouter = require('./routers/actions');
 
 const middleware = [
   helmet(),
@@ -25,53 +22,10 @@ server.get('/', (req, res) => {
   });
 });
 
-// POST Project
-server.post('/api/projects', validateBody({
-  name: {
-    type: 'string',
-    required: true,
-  },
-}), async (req, res) => {
-  try {
-    const project = await projectDB.add(req.body);
-    res.json(project);
-  } catch (error) {
-    res.status(500).json(errorRef(error));
-  }
-});
+// Project Router
+server.use('/api/projects', projectRouter);
 
-// POST Action
-server.post('/api/actions', validateBody({
-  description: {
-    type: 'string',
-    required: true,
-  },
-  project_id: {
-    type: 'number',
-    required: true,
-    exists: {
-      database: require('./data/models'),
-      table: 'project',
-      column: 'id',
-    }
-  }
-}), async (req, res) => {
-  try {
-    const action = await actionDB.add(req.body);
-    res.json(action);
-  } catch (error) {
-    res.status(500).json(errorRef(error));
-  }
-});
-
-// GET Project By Id
-server.get('/api/projects/:id', validateId(projectDB), async (req, res) => {
-  try {
-    const project = await projectDB.getProject(req.params.id);
-    res.json(project);
-  } catch (error) {
-    res.status(500).json(errorRef(err));
-  }
-});
+// Action Router
+server.use('/api/actions', actionRouter);
 
 server.listen(PORT, () => console.log(`Server is listening on port:${PORT}`));
